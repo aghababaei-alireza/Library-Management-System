401efaccbf06a6e26dbd9766838834f23b7e37ea
aghababaei-alireza <aghababaei1375@gmail.com>
2023-10-25 15:17:10 +0330

    2023-10-25-W

--- core/views.py
+++ core/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Category
 
 
 def home(request):
-    return render(request, 'core/base.html', {})
+    categories = [Category(name='Fantasy', description='Fantasy encompasses a huge part of the book world. It’s one of the most popular book genres out there—a personal favorite of mine to read and write.'),
+                  Category(
+                      name='Adventure', description='Writing a novel in the adventure category will require a trip, journey, or quest of some kind as the overall plot.'),
+                  Category(
+                      name='Contemporary', description='This book genre is among the most popular, though most writers aren’t sure of what this category even is.'),
+                  Category(
+                      name='Mystery', description='We’ve all heard of the mystery book genres. It’s an extremely popular genre, and for a good reason.'),
+                  Category(
+                      name='Horror', description='Horror novels are characterized by the fact that the main plot revolves around something scary and terrifying.'),
+                  Category(name='Romance', description='Romance authors have one specific goal when it comes to their books: to make you fall in love with the characters just as much as the characters fall in love with each other.')]
+    context = {
+        'categories': categories
+    }
+    return render(request, 'core/home.html', context)
